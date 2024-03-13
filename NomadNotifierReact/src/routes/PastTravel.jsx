495686bcda25299a0d0af3173981154992cf96a1
Miguel Palafox <102623518+mpala038@users.noreply.github.com>
2024-03-12 17:02:16 -0700

    Added home page button to PastTravel.jsx

--- NomadNotifierReact/src/routes/PastTravel.jsx
+++ NomadNotifierReact/src/routes/PastTravel.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Typewriter from "typewriter-effect";
+import '../styles/HomePage.css';
 
-const PastTravel = () => {
+function PastTravel () {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h2>Past Travels</h2>
+        <div className="HomePage">
+                <div className = "home_buttons">
+                    <button className="homepage_btn homepage-homepage_btn" onClick={() => navigate('/')}>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                            typewriter
+                            .changeDelay(60)
+                            .typeString("Nomad Notifier.")
+                            .start();
+                            }}
+                        />
+                    </button>
+                </div>
+
+             <div>
+                <h2>Past Travels</h2>
+            </div>
         </div>
-    );
+
+    )
 }
 
 export default PastTravel;
