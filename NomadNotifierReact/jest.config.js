179a266a0e810796fc4d6bd090e5ab6cc9b53602
jgonz671 <jgonz671@ucr.edu>
2024-03-14 14:10:13 -0700

    test suites for destination and signin

---
+++ NomadNotifierReact/jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['./jest.setup.cjs'],
+  testEnvironment: 'jest-environment-jsdom',
+};
